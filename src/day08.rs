a4bdc3ba5654366bff2ae899933f8afc192ad8a1
hilmi-yilmaz <hilmiyilmaz.1998@gmail.com>
2021-12-10 16:53:18 +0100

    setup for part8 done

---
+++ src/day08.rs
@@ -0,0 +1,9 @@
+use crate::get_data_from_input_file;
+
+pub fn day08(filename: &str) {
+
+	let input_data: Vec<String> = get_data_from_input_file(filename);
+
+	println!("input_data = {:?}", input_data);
+
+}
